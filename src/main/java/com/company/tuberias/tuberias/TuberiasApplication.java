f978dfaa61fc15d37c47c9a3d77b8b07193a1b1b
unknown <ruben310891@gmail.com>
2023-03-04 16:44:25 -0500

    proyecto base sistema tuberias

---
+++ src/main/java/com/company/tuberias/tuberias/TuberiasApplication.java
@@ -0,0 +1,13 @@
+package com.company.tuberias.tuberias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuberiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuberiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/tuberias/tuberias/TuberiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.tuberias.tuberias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuberiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
